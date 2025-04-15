2d2bf05271a0e854999285f1a2d51b5f0b692809
Roman Kolerov <coder8080@yandex.ru>
2025-04-15 12:07:02 +0300

    Add favicons

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -8,7 +8,26 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/divesea/favicon/favicon-96x96.png"
+            sizes="96x96"
+          />
+          <link
+            rel="icon"
+            type="image/svg+xml"
+            href="/divesea/favicon/favicon.svg"
+          />
+          <link rel="shortcut icon" href="/divesea/favicon/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/divesea/favicon/apple-touch-icon.png"
+          />
+          <meta name="apple-mobile-web-app-title" content="DiveSea" />
+          <link rel="manifest" href="/divesea/favicon/site.webmanifest" />
+          <meta name="theme-color" content="#ffffff" />
           {assets}
         </head>
         <body>
