376a6490f68965aaa23ea23bf6ef48bd90675287
Roman Kolerov <coder8080@yandex.ru>
2025-04-15 14:50:53 +0300

    Remove atropos on cards

--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -1,42 +1,18 @@
-import { createEffect, createUniqueId } from "solid-js";
 import Button from "~/shared/ui/Button";
 import Price from "~/shared/ui/Price";
-import Atropos from "atropos";
 
 export type Props = { title: string; timer: string; bid: number; img: string };
 
 const Card = (props: Props) => {
-  const id = createUniqueId();
-
-  createEffect(() => {
-    Atropos({
-      el: `.card-image-${id}`,
-      shadow: false,
-      activeOffset: 0,
-      rotateTouch: "scroll-y",
-    });
-  });
-
   return (
     <div class="p-[10px] lg:p-[14px] flex flex-col bg-white w-[200px] lg:w-[280px] rounded-3xl gap-[18px]">
-      <div class={`atropos card-image-${id}`}>
-        <div class="atropos-scale">
-          <div class="atropos-rotate">
-            <div class="atropos-inner">
-              <div
-                class=" relative aspect-square w-full rounded-3xl"
-                style={{
-                  "background-image": `url("/divesea/images/${props.img}")`,
-                  "background-size": "cover",
-                  "background-position": "center",
-                }}
-              >
-                <div class="absolute top-[10px] right-[8px] lg:top-[14px] lg:right-[10px] bg-[#1C1D2059]/65 rounded-xl px-[10px] py-[6px] lg:px-[14px] lg:py-[8px] text-white text-[14px] font-[Poppins] font-medium text-nowrap">
-                  {props.timer}
-                </div>
-              </div>
-            </div>
-          </div>
+      <div class="relative aspect-square w-full">
+        <img
+          src={`/divesea/images/${props.img}`}
+          class="object-cover w-full aspect-square rounded-3xl"
+        />
+        <div class="absolute top-[10px] right-[8px] lg:top-[14px] lg:right-[10px] bg-[#1C1D2059]/65 rounded-xl px-[10px] py-[6px] lg:px-[14px] lg:py-[8px] text-white text-[14px] font-[Poppins] font-medium text-nowrap">
+          {props.timer}
         </div>
       </div>
       <div class="flex flex-col gap-[14px]">
